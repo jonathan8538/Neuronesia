95206bc7a13d157c074d77df6b2a630d6562a226
jonathan8538 <jonathanliepku@gmail.com>
2025-11-20 09:25:10 +0700

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {}
+  },
+  // Force Vite to disable native Rollup binaries
+  experimental: {
+    disableRollupNative: true
+  }
 })
